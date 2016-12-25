90684c0d60bb8b6886138a48203d800757b15d03
binaryswitch <binaryswitch@jwhite.co.nz>
2016-12-25 15:29:24 +1300

    Fix undefined bug.

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ app.get('/board', function (req, res){
 
 app.get('/board/:boardId/pins', function (req, res){
     var jsonObj = {"pins": []};
-    jsonObj.boards.push({"id": 0, "type": "png"});
-    jsonObj.boards.push({"id": 1, "type": "png"});
+    jsonObj.pins.push({"id": 0, "type": "png"});
+    jsonObj.pins.push({"id": 1, "type": "png"});
 
     res.send(jsonObj);
 })
