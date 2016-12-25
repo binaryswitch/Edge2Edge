7ac0a74c37b58e70ce0252d1b51b2da68cead4af
binaryswitch <binaryswitch@jwhite.co.nz>
2016-12-25 15:07:44 +1300

    Add board route

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ app.get('/', function (req, res) {
   res.send('Edge2Edge server')
 })
 
+app.get('/board', function (req, res){
+    var jsonObj = {"boards": []};
+    jsonObj.boards.push({"id": 0, "name": "Board 0"});
+
+    res.send(jsonObj);
+})
+
 var port =  (process.env.PORT) ? process.env.PORT : 3001;
 
 app.listen(port, function () {
